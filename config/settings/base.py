b13e671f0f8bfa0f4d2167a822b5c24fc91f7709
Charles Changmin Han <name.cmh@gmail.com>
2020-04-11 13:27:09 -0700

    Create apps django app.

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,6 +76,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "wefocus.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "wefocus.apps.apps.AppsConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
