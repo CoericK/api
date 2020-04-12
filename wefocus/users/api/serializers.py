da7c8c42c98b928728edb678b993cfceabc81bb8
Charles Changmin Han <name.cmh@gmail.com>
2020-04-12 08:54:24 -0700

    Change party_id to party_slug.

--- wefocus/users/api/serializers.py
+++ wefocus/users/api/serializers.py
@@ -1,23 +1,26 @@
 from rest_framework import serializers
 
 from wefocus.users.models import User
-from wefocus.apps.parties.models import PartyMember
+from wefocus.apps.parties.models import Party, PartyMember
 
 
 class UserSerializer(serializers.ModelSerializer):
-    party_id = serializers.SerializerMethodField()
+    party_slug = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'name', 'url', 'party_id']
+        fields = ['username', 'email', 'name', 'url', 'party_slug']
 
         extra_kwargs = {
             'url': {'view_name': 'api:user-detail', 'lookup_field': 'username'}
         }
 
-    def get_party_id(self, obj):
+    def get_party_slug(self, obj):
         party_member_query = PartyMember.objects.filter(active=True)
         if party_member_query.exists():
-            return party_member_query.first().party_id
+            try:
+                return Party.objects.get(id=party_member_query.first().party_id).slug
+            except Party.DoesNotExist:
+                return None
         else:
-            None
+            return None
