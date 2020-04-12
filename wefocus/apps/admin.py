2112a81e1d801da8b5b6c670ae4289f9f9462694
Charles Changmin Han <name.cmh@gmail.com>
2020-04-12 09:45:54 -0700

    Add Party admin.

--- wefocus/apps/admin.py
+++ wefocus/apps/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .parties.models import Party, PartyMember
+from .timer.models import PomodoroTimer
+
+@admin.register(Party)
+class PartyAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'jitsi_id')
+    search_fields = ['slug']
